6c943d934da6f0ad6bd39fd93c195861a1e55e4f
xsanfz <sergey.myamin@gmail.com>
2025-02-21 16:17:18 +0400

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,83 @@
+import file_operations
+from faker import Faker
+import random
+import os
+
+# Инициализация Faker для русских данных
+fake = Faker("ru_RU")
+
+# Список навыков
+skills = [
+    'Стремительный прыжок',
+    'Электрический выстрел',
+    'Ледяной удар',
+    'Стремительный удар',
+    'Кислотный взгляд',
+    'Тайный побег',
+    'Ледяной выстрел',
+    'Огненный заряд'
+]
+
+# Маппинг для замены символов на "рунический" шрифт
+letters_mapping = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+# Функция для замены символов в тексте
+def replace_font(text, mapping):
+    return ''.join([mapping.get(char, char) for char in text])
+
+# Применяем замену шрифта к каждому навыку
+runic_skills = [replace_font(skill, letters_mapping) for skill in skills]
+
+
+
+# Создаем и записываем 10 разных файлов
+for i in range(1, 11):
+    skills_new = random.sample(runic_skills, 3)  # Выбираем 3 случайных навыка
+    context = {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "job": fake.job(),
+        "town": fake.city(),
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+        "skill_1": skills_new[0],
+        "skill_2": skills_new[1],
+        "skill_3": skills_new[2]
+    }
+    # Создаем директорию, если она не существует
+    directory = "output/svg/"
+    os.makedirs(directory, exist_ok=True)
+    # Формируем имя файла
+    filename = os.path.join(directory, f"result_{i}.svg")
+
+    # Рендерим шаблон и записываем результат в файл
+    file_operations.render_template("src/charsheet.svg", "output/svg/result.svg", context)
+
+    print(f"Файл {filename} создан и записан.")
