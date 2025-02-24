228fc3ae7f75ae864e164b987eee039dbcb20be1
xsanfz <sergey.myamin@gmail.com>
2025-02-24 17:40:12 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import file_operations
 from faker import Faker
 fake = Faker("ru_RU")
 import random
+import os
+
+
+
 
 skills = [
           'Стремительный прыжок',
-          'Электрический выстрел'
+          'Электрический выстрел',
           'Ледяной удар',
           'Стремительный удар',
           'Кислотный взгляд',
@@ -13,14 +17,6 @@ skills = [
           'Ледяной выстрел',
           'Огненный заряд'
           ]
-
-str_list = " ".join(skills)
-new_str_list = str_list.replace("е", "е͠")
-new_skills_1 = new_str_list.split()
-text = skills[0]
-for letter in text:
-    for number in range(10):
-  print(letter)
 
 letters_mapping = {
     'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
@@ -49,24 +45,74 @@ letters_mapping = {
 }
 
 
+def replace_font(text, mapping):
+    return ''.join([mapping.get(char, char) for char in text])
+
+
+new_skills = [replace_font(skill, letters_mapping) for skill in skills]
+runic_skills = []
+
+for skill in new_skills:
+    runic_skills.append(skill)
+
+for i in range(1, 11):
+    skills_new = random.sample(runic_skills, 3)
+
+    # Контекст для рендеринга
+    context = {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "job": fake.job(),
+        "town": fake.city(),
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+        "skill_1": skills_new[0],
+        "skill_2": skills_new[1],
+        "skill_3": skills_new[2]
+    }
+
+    rendered_template = file_operations.render_template("src/charsheet.svg", "result.svg", context)
+    ## Сохранение результата в файл
+    filename = os.path.join("output/src", f"result_{i}.svg")
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(rendered_template)
+
+    print(f"Файл {filename} создан и записан.")
+    # Рендеринг шаблона
+    file_operations.render_template("src/charsheet.svg", "result.svg", context)
 
 
 
-
-skills_new = random.sample(new_skills_1, 3)
-
-context = {
-  "first_name": fake.first_name(),
-  "last_name": fake.last_name(),
-  "job": fake.job(),
-  "town": fake.city(),
-  "strength": random.randint(3, 18),
-  "agility": random.randint(3, 18),
-  "endurance": random.randint(3, 18),
-  "intelligence": random.randint(3, 18),
-  "luck": random.randint(3, 18),
-  "skill_1": skills_new[0],
-  "skill_2": skills_new[1],
-  "skill_3": skills_new[2]
-}
-file_operations.render_template("src/charsheet.svg", "output/svg/result.svg", context)
+# # Создаем директорию, если она не существует
+# directory = "output/svg/"
+# os.makedirs(directory, exist_ok=True)
+#
+# # Создаем и записываем 10 разных файлов
+# for i in range(1, 11):
+#     skills_new = random.sample(runic_skills, 3)
+#     filename = os.path.join(f"result_{i}.svg")
+#     print(f"Файл {filename} создан и записан.")
+# context = {
+#   "first_name": fake.first_name(),
+#   "last_name": fake.last_name(),
+#   "job": fake.job(),
+#   "town": fake.city(),
+#   "strength": random.randint(3, 18),
+#   "agility": random.randint(3, 18),
+#   "endurance": random.randint(3, 18),
+#   "intelligence": random.randint(3, 18),
+#   "luck": random.randint(3, 18),
+#   "skill_1": skills_new[0],
+#   "skill_2": skills_new[1],
+#   "skill_3": skills_new[2]
+# }
+#
+#
+#
+# file_operations.render_template("src/charsheet.svg", "result.svg", context)
+#
+#
+#
